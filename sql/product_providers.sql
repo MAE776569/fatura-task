1405b32dce9455c761351b2a0ca9e7c4921ff0cc
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2021-04-07 18:03:12 +0200

    Fix product provider reference

--- sql/product_providers.sql
+++ sql/product_providers.sql
@@ -1,9 +1,9 @@
 CREATE TABLE product_providers(
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   product_id INT NOT NULL,
-  category_id INT NOT NULL,
+  provider_id INT NOT NULL,
   price DOUBLE NOT NULL,
   available BOOLEAN DEFAULT TRUE,
   FOREIGN KEY (product_id) REFERENCES products(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id)
+  FOREIGN KEY (provider_id) REFERENCES providers(id)
 );
